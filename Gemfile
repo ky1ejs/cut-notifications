9d976b780ab79245ca3dd9ca30dce550f7522a67
Kyle McAlpine <kyle@loot.io>
2017-05-08 22:58:14 +0100

    Using connection_pool for APNS

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 # Push notifications
 gem 'houston'
 
+# For a pool of APNS connections
+gem 'connection_pool'
+
 # Consuming RabbitMQ
 gem 'sneakers'
 
--- notification_worker.rb
+++ notification_worker.rb
@@ -1,45 +1,70 @@
 require 'sneakers'
 require 'json'
 require 'houston'
+require 'connection_pool'
 
 class NotificationWorker
   include Sneakers::Worker
   from_queue "notification.queue", env: nil
 
-  def self.ios_dev_connecton
-    return @ios_dev_connecton if !@ios_dev_connecton.nil?
-    certificate = File.read(ENV['CUT_APNS_DEV_PEM'])
-    passphrase = ''
-    @ios_dev_connecton = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, nil)
-    @ios_dev_connecton.open
-    return @ios_dev_connecton
+  DEV_APNS_POOL = ConnectionPool.new(:size => 5, :timeout => 300) do
+    pem_path = ENV['CUT_APNS_DEV_PEM']
+    puts pem_path
+    certificate = File.read(pem_path)
+    conn = Houston::Connection.new(Houston::APPLE_DEVELOPMENT_GATEWAY_URI, certificate, '')
+    conn.open
+    conn
   end
 
-  def self.ios_prod_connecton
-    return @ios_prod_connecton if !@ios_prod_connecton.nil?
-    certificate = File.read(ENV['CUT_APNS_PROD_PEM'])
-    passphrase = ''
-    @ios_prod_connecton = Houston::Connection.new(Houston::APPLE_PRODUCTION_GATEWAY_URI, certificate, passphrase)
-    @ios_prod_connecton.open
-    return @ios_prod_connecton
+  PROD_APNS_POOL = ConnectionPool.new(:size => 5, :timeout => 300) do
+    pem_path = ENV['CUT_APNS_PROD_PEM']
+    puts pem_path
+    certificate = File.read(pem_path)
+    conn = Houston::Connection.new(Houston::APPLE_PRODUCTION_GATEWAY_URI, certificate, '')
+    conn.open
+    conn
   end
 
   def work(raw_event)
     payload = JSON.parse raw_event
 
-    notification = Houston::Notification.new(device: payload['token'])
-    notification.alert = payload['message']
+    token = payload['token']
+    message = payload['message']
+    is_dev_token = payload['is_dev_token']
 
-    # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
-    notification.badge = 0
-    notification.sound = 'default'
-    # notification.category = ''
-    # notification.content_available = true
-    # notification.mutable_content = true
-    # notification.custom_data = { foo: 'bar' }
+    if token == nil ||
+      !token.is_a? String ||
+      message == nil ||
+      !message.is_a? String ||
+      is_dev_token == nil ||
+      !(is_dev_token.is_a? TrueClass || is_dev_token.is_a? FalseClass)
+      ack!
+      puts '################################'
+      puts "JSON payload invalid"
+      puts "token         - #{token}"
+      puts "message       - #{message}"
+      puts "is_dev_token  - #{is_dev_token}"
+      puts '################################'
+      return
+    end
 
-    conn = payload['is_dev_token'] ? self.class.ios_dev_connecton : self.class.ios_prod_connecton
-    conn.write(notification.message)
+    pool = is_dev_token ? DEV_APNS_POOL : PROD_APNS_POOL
+
+    pool.with do |conn|
+      notification = Houston::Notification.new(device: token)
+      notification.alert = message
+
+      # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
+      notification.badge = 0
+      notification.sound = 'default'
+      # notification.category = ''
+      # notification.content_available = true
+      # notification.mutable_content = true
+      # notification.custom_data = { foo: 'bar' }
+
+      puts conn.write(notification.message)
+      puts notification.error
+    end
 
     ack!
   end
